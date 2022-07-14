1ac57cab0b267bd47db3a717ff28063f82d6db49
Giovane <giovanedaniell@gmail.com>
2022-07-14 16:59:49 -0300

    implementação do switch component no browser router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Foods from './pages/Foods';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={ Login } />
-      <Route path="/foods" exact component={ Foods } />
+      <Switch>
+        <Route path="/" exact component={ Login } />
+        <Route path="/foods" exact component={ Foods } />
+      </Switch>
     </BrowserRouter>
   );
 }
