f32271cb74510925864fe0f0073a4b57d0ebb956
Giovane <giovanedaniell@gmail.com>
2022-07-14 17:06:32 -0300

    implementação de todas as rotas no App

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={ Login } />
-        <Route path="/foods" exact component={ Foods } />
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/foods" component={ Foods } />
+        <Route exact path="/foods/:id" />
+        <Route exact path="/foods/:id/in-progress" />
+        <Route exact path="/drinks" />
+        <Route exact path="/drinks/:id" />
+        <Route exact path="/drinks/:id/in-progress" />
+        <Route exact path="/done-recipes" />
+        <Route exact path="/favorite-recipes" />
+        <Route exact path="/profile" />
       </Switch>
     </BrowserRouter>
   );
