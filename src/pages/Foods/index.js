799ebcc167ebda39489025f94008570fb89d4ff5
Giovane <giovanedaniell@gmail.com>
2022-07-15 16:15:50 -0300

    req 14

--- src/pages/Foods/index.js
+++ src/pages/Foods/index.js
@@ -1,23 +1,50 @@
 import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import RecipeCard from '../../components/RecipeCard';
 import Header from '../../components/Header';
 import { FoodsContext } from '../../context/FoodContext';
+import './Foods.css';
 
 export default function Foods() {
-  const { recipes } = useContext(FoodsContext);
+  const { recipes, saveRecipes } = useContext(FoodsContext);
   const history = useHistory();
 
   useEffect(() => {
-    if (recipes.length === 1) {
+    if (recipes && recipes.length === 1) {
       const mealId = recipes[0].idMeal;
       history.push(`/foods/${mealId}`);
     }
+
+    if (!recipes) {
+      global.alert(`${''}Sorry, we haven't found any recipes for these filters.`);
+      saveRecipes([]);
+    }
   }, [recipes]);
+
+  function filterRecipes(array) {
+    if (!array) return;
+
+    const maxRecipesArrayLenght = 12;
+    if (array.length > maxRecipesArrayLenght) {
+      return array.slice(0, maxRecipesArrayLenght);
+    }
+
+    return array;
+  }
 
   return (
     <main className="foods-page-container">
       <Header title="Foods" haveSearch />
       <h1>Foods page</h1>
+      <div className="food-cards-container">
+        {!!recipes && filterRecipes(recipes).map((recipe, index) => (
+          <RecipeCard
+            key={ recipe.strMeal }
+            recipe={ recipe }
+            index={ index }
+          />
+        ))}
+      </div>
     </main>
   );
 }
