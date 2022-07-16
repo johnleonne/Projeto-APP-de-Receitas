7342eff613d5934abffa20091e73c0511964f18e
Lucas Monteiro <lucas.ferreira4261@gmail.com>
2022-07-16 12:46:12 -0300

    Testes Details

--- src/tests/DrinkDetails.test.js
+++ src/tests/DrinkDetails.test.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { DrinkDetail } from '../pages';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
 
 describe('Testes para a página de perfil', () => {
-  it('Verifica se o header renderiza com as informações corretas', () => {
-    render(<DrinkDetail />);
+  it('Verifica o redirecionamento para a página de detalhes da bebida', async () => {
+    render(<App />);
 
-    expect(screen.queryByTestId('header')).toBeFalsy();
-    expect(screen.queryByTestId('profile-top-btn')).toBeFalsy();
-    expect(screen.queryByTestId('search-top-btn')).toBeFalsy();
+    const nameInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const loginBtn = screen.getByTestId('login-submit-btn');
+
+    userEvent.type(nameInput, 'teste@gmail.com');
+    userEvent.type(passwordInput, '123456789');
+    userEvent.click(loginBtn);
+
+    expect(screen.getByText('Foods')).toBeInTheDocument();
+
+    const footerDrinkBtnRedirect = screen.getByTestId('drinks-bottom-btn');
+
+    userEvent.click(footerDrinkBtnRedirect);
+
+    expect(screen.getByText('Drinks')).toBeInTheDocument();
+
+    const searchBtn = screen.getByTestId('search-top-btn');
+    userEvent.click(searchBtn);
+
+    const searchInput = screen.getByTestId('search-input');
+    const nameRadio = screen.getByLabelText(/name/i);
+    userEvent.type(searchInput, 'abc')
+    userEvent.click(nameRadio);
+
+    const filterSearchBtn = screen.getByTestId('exec-search-btn');
+    userEvent.click(filterSearchBtn);
+
+    expect(await screen.findByText(/drink detail page/i)).toBeInTheDocument();
+
   });
 });
--- src/tests/FoodDetail.test.js
+++ src/tests/FoodDetail.test.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { FoodDetail } from '../pages';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
 
 describe('Testes para a página de perfil', () => {
-  it('Verifica se o header renderiza com as informações corretas', () => {
-    render(<FoodDetail />);
+  it('Verifica o redirecionamento para a página de detalhes da bebida', async () => {
+    render(<App />);
 
-    expect(screen.queryByTestId('header')).toBeFalsy();
-    expect(screen.queryByTestId('profile-top-btn')).toBeFalsy();
-    expect(screen.queryByTestId('search-top-btn')).toBeFalsy();
+    const nameInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const loginBtn = screen.getByTestId('login-submit-btn');
+
+    userEvent.type(nameInput, 'teste@gmail.com');
+    userEvent.type(passwordInput, '123456789');
+    userEvent.click(loginBtn);
+
+    expect(screen.getByText('Foods')).toBeInTheDocument();
+
+    const searchBtn = screen.getByTestId('search-top-btn');
+    userEvent.click(searchBtn);
+
+    const searchInput = screen.getByTestId('search-input');
+    const nameRadio = screen.getByLabelText(/name/i);
+    userEvent.type(searchInput, 'Apam balik')
+    userEvent.click(nameRadio);
+
+    const filterSearchBtn = screen.getByTestId('exec-search-btn');
+    userEvent.click(filterSearchBtn);
+
+    expect(await screen.findByText(/food detail page/i)).toBeInTheDocument();
+
   });
 });
