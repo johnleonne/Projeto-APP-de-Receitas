c71c2c8c1148d07be449e97f613f16f923b4e733
Giovane <giovanedaniell@gmail.com>
2022-07-18 11:25:09 -0300

    req 28 - sem testes

--- src/components/StartRecipeButton/index.js
+++ src/components/StartRecipeButton/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import useLocalStorage from '../../hooks/useLocalStorage';
 import './StartRecipeButton.css';
 
-export default function StartRecipeButton() {
+export default function StartRecipeButton({ recipe, recipeType }) {
+  const [doneRecipes] = useLocalStorage('doneRecipes');
+
+  if (doneRecipes?.find((localStorageRecipe) => (
+    Number(localStorageRecipe.id) === Number(recipe.idMeal ?? recipe.idDrink)
+  ))) return null;
+
   return (
     <button
       type="button"
       data-testid="start-recipe-btn"
       className="start-recipe-button"
+      onClick={ () => console.log(recipe, recipeType) }
     >
       Start Recipe
     </button>
   );
 }
+
+StartRecipeButton.propTypes = {
+  recipe: PropTypes.shape().isRequired,
+  recipeType: PropTypes.string.isRequired,
+};
---
+++ src/utils/createDoneRecipeObject.js
@@ -0,0 +1,13 @@
+export default function createDoneRecipeObject(recipe, type) {
+  return ({
+    id: recipe.idDrink ?? recipe.idMeal,
+    type: type === '/foods' ? 'food' : 'drink',
+    nationality: recipe.strArea ?? '',
+    category: recipe.strCategory ?? '',
+    alcoholicOrNot: recipe.strAlcoholic ?? '',
+    name: recipe.strDrink ?? recipe.strMeal,
+    image: recipe.strDrinkThumb ?? recipe.strMealThumb,
+    doneDate: new Date(Date.now()).toLocaleDateString('pt-BR', { timeZone: 'UTC' }),
+    tags: strTags?.split(',') ?? [],
+  });
+}
