5716a46d8450c763ebb2fd3c99b6dfc87572deb7
Lucas Monteiro <lucas.ferreira4261@gmail.com>
2022-07-18 17:46:19 -0300

    Testes Food Details

--- src/tests/FoodDetail.test.js
+++ src/tests/FoodDetail.test.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import { fireEvent, screen, waitFor } from '@testing-library/react';
-import App from '../App';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouter from './helpers/renderWithRouter'
-
+import renderWithRouter from './helpers/renderWithRouter';
 
 describe('Testes para a página de Foods', () => {
   it('Verifica o redirecionamento para a página de detalhes da Foods', async () => {
@@ -19,5 +16,20 @@ describe('Testes para a página de Foods', () => {
     expect(screen.getByTestId('share-btn')).toBeInTheDocument()
     userEvent.click(screen.getByTestId('share-btn'))
     expect(screen.getByText('Link copied!')).toBeInTheDocument()
-  });  
+  });
+
+  it('Testa a funcionalidade do botão "Iniciar Receita"',async () => {
+    const { history } = renderWithRouter('/foods/52795');
+
+    jest.spyOn(Storage.prototype, 'setItem');
+    Storage.prototype.setItem = jest.fn();
+
+    userEvent.click(await screen.findByTestId('start-recipe-btn'));
+
+    expect(await screen.findByRole('heading',{ name: /Food in progress details page/i})).toBeInTheDocument();
+  
+    const favoriteRecipeObj = createFavoriteRecipeObj(favMeal, 'meal');
+    expect(localStorage.setItem).toHaveBeenNthCalledWith('favoriteRecipes', JSON.stringify([favoriteRecipeObj]));
+
+  })
 });
