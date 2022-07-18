00df4e962afebd99401c06ff85cc2cb26cf648c5
Giovane <giovanedaniell@gmail.com>
2022-07-18 13:48:15 -0300

    fix req 33

--- src/components/RecipeDetailsInteractions/index.js
+++ src/components/RecipeDetailsInteractions/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clipboardCopy from 'clipboard-copy';
 
 export default function RecipeDetailsInteractions() {
+  const [isLinkCopied, setIsLinkCopied] = useState(false);
+
+  function handleShare() {
+    setIsLinkCopied(true);
+    clipboardCopy(window.location.href);
+  }
+
   return (
     <div className="interaction-buttons">
       <button
         type="button"
         data-testid="share-btn"
+        onClick={ handleShare }
       >
         Share
       </button>
+      { isLinkCopied && <p>Link copied!</p>}
       <button
         type="button"
         data-testid="favorite-btn"
