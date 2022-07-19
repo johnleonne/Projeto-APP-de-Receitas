cc80e824d16634910e8da4d15da5a6d3d35dc6a0
costalucs <lucasdeslz@hotmail.com>
2022-07-19 11:14:59 -0300

    profile with tests

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer/Footer';
 
 export default function Profile() {
+  const [email, setEmail] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      setEmail(user.email);
+    }
+  }, []);
+
+  function goHome() {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
     <main className="profile-page-container">
       <Header title="Profile" />
       <h1>Profile page</h1>
+      <h3 data-testid="profile-email">{email}</h3>
+      <div>
+        <button
+          onClick={ () => history.push('/done-recipes') }
+          data-testid="profile-done-btn"
+          type="button"
+        >
+          Done Recipes
+        </button>
+        <button
+          onClick={ () => history.push('/favorite-recipes') }
+          data-testid="profile-favorite-btn"
+          type="button"
+        >
+          Favorite Recipes
+
+        </button>
+        <button
+          onClick={ goHome }
+          data-testid="profile-logout-btn"
+          type="button"
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </main>
   );
